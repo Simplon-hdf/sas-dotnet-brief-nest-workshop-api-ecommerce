0600dc28c7f760b58788f30ced75643ff76309af
massima <mastem@msn.com>
2023-12-06 16:41:22 +0100

    feat(orderResource): config order service get and remove by id

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaService } from 'src/prisma.service';
+import NormalizedResponse from 'utils/noramlized-response';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
+    constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all orders`;
-  }
+    public async create(createOrderDto: CreateOrderDto) {
+        const createOrder = new NormalizedResponse(
+            `Order n° ${createOrderDto.id} has been created`,
+            await this.prisma.orders.create({
+                data: {
+                    id: createOrderDto.id,
+                    product_quantity: createOrderDto.product_quantity,
+                    total_price: createOrderDto.total_price,
+                    order_date: createOrderDto.order_date,
+                    shipping_date: createOrderDto.shipping_date,
+                },
+            }),
+        ).toJSON();
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
+    findAll() {
+        return `This action returns all orders`;
+    }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
+    public async getById(id: number) {
+        const gettedOrder = new NormalizedResponse(
+            `Order ${id} has been found`,
+            await this.prisma.orders.findUnique({
+                where: {
+                    id: id,
+                },
+            }),
+        ).toJSON();
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
+    public async updateById(id: number, updateOrderDto: UpdateOrderDto) {}
+
+    public async removeById(id: number) {
+        const removeOrder = new NormalizedResponse(
+            `Order n° ${id} has been deleted`,
+            await this.prisma.orders.delete({
+                where: {
+                    id: id,
+                },
+            }),
+        ).toJSON();
+
+        return `This action removes a #${id} order`;
+    }
 }
