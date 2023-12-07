546b27c6524672f1984c66ce882e0aa19a28e00f
massima <mastem@msn.com>
2023-12-07 12:06:03 +0100

    fix(format): npm run format command

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
+    }
 
-  // @Get()
-  // findAllBYUUID('uuid') {
-  //   return this.usersService.findAllByUUID(uuid);
-  // }
+    // @Get()
+    // findAllBYUUID('uuid') {
+    //   return this.usersService.findAllByUUID(uuid);
+    // }
 
-  @Get()
-  getByUUID(@Param('uuid') uuid: string) {
-    return this.usersService.getByUUID(uuid);
-  }
+    @Get()
+    getByUUID(@Param('uuid') uuid: string) {
+        return this.usersService.getByUUID(uuid);
+    }
 
-  @Patch(':uuid')
-  updateByUUID(@Param('uuid') uuid: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.updateByUUID(uuid, updateUserDto);
-  }
+    @Patch(':uuid')
+    updateByUUID(
+        @Param('uuid') uuid: string,
+        @Body() updateUserDto: UpdateUserDto,
+    ) {
+        return this.usersService.updateByUUID(uuid, updateUserDto);
+    }
 
-  @Delete(':uuid')
-  removeByUUID(@Param('uuid') uuid: string) {
-    return this.usersService.removeByUUID(uuid);
-  }
+    @Delete(':uuid')
+    removeByUUID(@Param('uuid') uuid: string) {
+        return this.usersService.removeByUUID(uuid);
+    }
 }
