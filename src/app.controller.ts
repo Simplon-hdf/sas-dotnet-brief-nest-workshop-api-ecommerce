9dcd47d165499ae2d8379379bf800d35b6bfda5c
Remid <remidebruyne@hotmail.com>
2023-12-15 13:53:09 +0100

    feat(video): add video

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
+
+    @Get()
+    rickrolled() {
+        return fs.readFileSync('./src/front/index.html', 'utf-8');
+    }
 }
